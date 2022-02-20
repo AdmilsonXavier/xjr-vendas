0eaa65e4b9e0b35af8b9df5181d7e2314359deb8
Admilson Xavier <admilson.xjr@gmail.com>
2022-02-20 00:42:38 -0300

    Project Created

---
+++ backend/src/test/java/com/admilson/xjrvendas/XjrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admilson.xjrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XjrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
