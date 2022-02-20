0dbd086039460e5d255fb9645621d6e5c38dcce9
Admilson Xavier <admilson.xjr@gmail.com>
2022-02-20 15:04:30 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/AdmilsonXavier" target="_blank" rel="noreferrer">Admilson Xavier</a></p>
+          <p className="text-light"><small><strong>ADM XJR</strong><br/></small></p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
