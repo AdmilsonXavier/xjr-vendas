f0cd7a21f73e5a8684e71fa165d9fe7a4eb2b692
Admilson Xavier <admilson.xjr@gmail.com>
2022-02-23 23:05:55 -0300

    Pagination

---
+++ backend/src/main/java/com/admilson/xjrvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.admilson.xjrvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.admilson.xjrvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/admilson/xjrvendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.admilson.xjrvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.admilson.xjrvendas.dto.SaleDTO;
+import com.admilson.xjrvendas.entities.Sale;
+import com.admilson.xjrvendas.repositories.SaleRepository;
+import com.admilson.xjrvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pegeable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pegeable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
